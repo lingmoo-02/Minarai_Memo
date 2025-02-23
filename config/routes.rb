a2ee1b412b3fb73a47bec5196c6d68f68521ea6f
鈴木晃一 <suzukikouichi@Suzuki.local>
2025-02-24 02:45:38 +0900

    トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#top'
   resources :notes
-  root 'notes#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
