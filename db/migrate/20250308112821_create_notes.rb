e2331ea941936c6dda1ea65ca7867d75cb7063d2
鈴木晃一 <suzukikouichi@Suzuki.local>
2025-03-08 20:54:14 +0900

    マイグレーションファイルの順序を適切に変更

--- db/migrate/20250308112821_create_notes.rb
+++ db/migrate/20250308112821_create_notes.rb
@@ -1,9 +1,9 @@
 class CreateNotes < ActiveRecord::Migration[7.2]
   def change
     create_table :notes do |t|
-      t.references :user, foreign_key: true
-      t.string :title, null: false
-      t.text :body, null: false
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.text :body
 
       t.timestamps
     end
