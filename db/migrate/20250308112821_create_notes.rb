00ec32b33c6b4de9d80de5015892364ad262957f
鈴木晃一 <suzukikouichi@Suzuki.local>
2025-03-10 00:08:29 +0900

    マイグレーションファイルの適用

--- db/migrate/20250308112821_create_notes.rb
+++ db/migrate/20250308112821_create_notes.rb
@@ -1,5 +1,7 @@
 class CreateNotes < ActiveRecord::Migration[7.2]
   def change
+    return if ActiveRecord::Base.connection.table_exists?(:notes)
+    
     create_table :notes do |t|
       t.references :user, null: false, foreign_key: true
       t.string :title
