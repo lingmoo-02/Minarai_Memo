4ed573d8fdfa4b3a704b7ea42469b229ef8f8ab4
鈴木晃一 <suzukikouichi@Suzuki.local>
2025-03-15 00:59:15 +0900

    アセットパイプラインの本番環境の設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_08_112821) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_13_151219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_08_112821) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "note_image"
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
