fcf141393e8ad30e9126a372f8930cbeff0cf9f5
鈴木晃一 <suzukikouichi@Suzuki.local>
2025-06-30 18:24:12 +0900

    マイグレーションの生成、config、コントローラー、モデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         :omniauthable, omniauth_providers: [:google_oauth2]
+  
+  has_many :notes, dependent: :destroy
   
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 
-  has_many :notes, dependent: :destroy
+  validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+
 end
