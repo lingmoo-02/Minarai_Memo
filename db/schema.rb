1b82a213fbda80d5c903aa975c7ae35f9e98573e
lingmoo-02 <runteq.suzuki.koichi@gmail.com>
2026-01-27 11:03:18 +0900

    added file: db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_17_000011) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_27_014651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "note_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["note_id"], name: "index_bookmarks_on_note_id"
+    t.index ["user_id", "note_id"], name: "index_bookmarks_on_user_id_and_note_id", unique: true
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "notes", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -75,6 +85,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_17_000011) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "notes"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "notes", "tags"
   add_foreign_key "notes", "teams"
   add_foreign_key "notes", "users"
